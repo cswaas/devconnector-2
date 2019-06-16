58aaf1d82564613de2167609f26dc0c1ff1e85e3
Christian Waas <cswaas@gmail.com>
2019-06-16 18:18:42 +0200

    Video 56 Starting On Th Profile commit

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -48,13 +48,13 @@ export const getProfiles = () => async dispatch => {
   }
 };
 
-// Get  profiles by ID
+// Get  profile by ID
 export const getProfileById = userId => async dispatch => {
   try {
-    const res = await axios.get(`/api/profile/user${userId}`);
+    const res = await axios.get(`/api/profile/user/${userId}`);
 
     dispatch({
-      type: GET_PROFILES,
+      type: GET_PROFILE,
       payload: res.data
     });
   } catch (err) {
