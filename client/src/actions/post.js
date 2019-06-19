1dc432dd3b19e3b03264f12136f42bbd4e136c56
Christian Waas <cswaas@gmail.com>
2019-06-19 18:40:26 +0200

    Video 64 Single Post Display commit

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -80,3 +80,27 @@ export const deletePost = id => async dispatch => {
     });
   }
 };
+
+// Add post
+export const addPost = formData => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  try {
+    const res = await axios.post('/api/posts/', formData, config);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data
+    });
+
+    dispatch(setAlert('Post Created', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
