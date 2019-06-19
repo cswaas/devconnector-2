292b21019ba0da4d740367a69303e3b313655c01
Christian Waas <cswaas@gmail.com>
2019-06-19 19:38:37 +0200

    Section 11 Posts & Comments commit

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -22,6 +22,11 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       </Link>
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
+      <div className="comments">
+        {post.comments.map(comment => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
