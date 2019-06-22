5e79aebd6022c2405c643cd6c301b34bbbf43b8c
Christian Waas <cswaas@gmail.com>
2019-06-22 10:56:21 +0200

    Correction to Page Not Found commit

--- client/src/App.js
+++ client/src/App.js
@@ -26,8 +26,10 @@ const App = () => {
       <Router>
         <Fragment>
           <Navbar />
-          <Route exact path="/" component={Landing} />
-          <Route component={Routes} />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route component={Routes} />
+          </Switch>
         </Fragment>
       </Router>
     </Provider>
